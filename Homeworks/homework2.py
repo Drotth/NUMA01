023b53bfb8eac08ac22d6e95776749f4415d4ed2
sebo1 <sebbe_olsson@live.se>
2015-12-14 22:09:28 +0100

    big update

--- Homeworks/homework2.py
+++ Homeworks/homework2.py
@@ -13,6 +13,7 @@ class Interval(object):
         Init (initialize) function is the first function called when an object creates
         Self represent the instance of the object itself
         """
+        
         self.leftendpoint=leftendpoint
         self.rightendpoint=rightendpoint
         start=[self.leftendpoint,self.rightendpoint]
@@ -75,43 +76,90 @@ class Interval(object):
        
     def __iadd__(self, other):
         iadd=[]
-        self.leftendpoint += other.rightendpoint
-        self.rightendpoint += other.leftendpoint
+        self.leftendpoint += other
+        self.rightendpoint += other
         iadd.append(self.rightendpoint)
         iadd.append(self.leftendpoint)
         return iadd    
 
     def __pow__(self, other):
-        if n%2==0 and n>0:#even
-            print("s")
-        else:
-            if self.rightendpoint>a or self.rightendpoint==a:
-            elif self.leftendpoint<0:
+        """
+        This function is used for the power of
+        """              
+        powe=[]
+        if other%2==0 and other>0:#even
+            if self.leftendpoint>=0:
+                powe.append(self.rightendpoint**other)
+                powe.append(self.leftendpoint**other)
+            elif self.rightendpoint<0:
+                powe.append(self.rightendpoint**other)
+                powe.append(self.leftendpoint**other)
             else:    
-            pow(self.leftendpoint,n)
-            pow(self.rightendpoint,n)
-   
-I1 = Interval(1,2) # [1, 2]
-I2 = Interval(3, 4) # [3, 4]
-print("Reslults:")
-I3=I1 + I2 # [4, 6]
-print(I3)
-I4=I1 - I2 # [-3, -1]
-print(I4)
-I5=I1 * I2 # [3, 8]
-print(I5)
-I6=I1 / I2 # [0.25,0.6666666666666666]
-print(I6)
+                powe.append(0)
+                powe.append(max(self.leftendpoint**other,self.rightendpoint**other))
+        else:
+            powe.append(self.leftendpoint**other)
+            powe.append(self.rightendpoint**other)
+                  
+        return powe
+                
+    def __contains__(self):
+        if not isinstance(self.leftendpoint,int) or isinstance(self.leftendpoint,float):
+            raise TypeError('Left endpoint is not real value')
+        if not isinstance(self.rightendpoint,int) or isinstance(self.rightendpoint,float) :
+            raise TypeError('Right endppoint is not real balue')
+    
+    def plot_values():
+        """
+        Evaluate the polynomial p(x)=3x^3−2x^2+5x−1
+        Plots lower boundaries and upper boundaries for y with respect to 
+        lower boundaries of x
+        """
+        yl=[]
+        yu=[]
+        xl=linspace(0.,1,1000)
+        xu=linspace(0.,1,1000)+0.5
+        p=[3,-2,5,-1]
+        yl=np.polyval(p,xl)
+        yu=np.polyval(p,xu)
+        plot(xl, yl, label='yl') 
+        plot(xl, yu, label='yu')
+        xlabel('xl')
+        ylabel('y')
+        title("Graph")
+        legend ()
+        show()        
+                
+if __name__ == '__main__':                
+      
+    I1 = Interval(1,2) # [1, 2]
+    I2 = Interval(3, 4) # [3, 4]
+    print("Reslults:")
+    I3=I1 + I2 # [4, 6]
+    print(I3)
+    I4=I1 - I2 # [-3, -1]
+    print(I4)
+    I5=I1 * I2 # [3, 8]
+    print(I5)
+    I6=I1 / I2 # [0.25,0.6666666666666666]
+    print(I6)
+    
+    
+    """
+    I7=Interval(2,3) + 1 # [3, 4]
+    #print(I7)
+    1 + Interval(2,3) # [3, 4]
+    1.0 + Interval(2,3) # [3.0, 4.0]
+    """  
 
-"""
-I7=Interval(2,3) + 1 # [3, 4]
-#print(I7)
-1 + Interval(2,3) # [3, 4]
-1.0 + Interval(2,3) # [3.0, 4.0]
-"""  
-"""
-x = Interval(-2,2) # [-2, 2]
-x**2 # [0, 4]
-x**3 # [-8, 8]
-"""
+    x = Interval(-2,2) # [-2, 2]
+    print(x**2) # [0, 4]
+    print(x**3) # [-8, 8]
+
+    """
+    I9 = Interval(1j,3)
+    I9.__contains__()
+    """
+
+    Interval.plot_values()
 
