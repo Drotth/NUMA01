6e4f0172e6c3569dee375e7a1ef128c8caba1086
Andreas Drotth <drotth@hotmail.com>
2016-01-12 22:47:25 +0100

    Trying to integrate task 3

--- Project/project_3_birds.py
+++ Project/project_3_birds.py
@@ -8,6 +8,7 @@ from pylab import *
 from datetime import datetime
 from datetime import timedelta
 import pytz
+from preprocess import preprocessing
 
 list_dates = []
 list_data = []
@@ -37,62 +38,6 @@ def convert_local_timezone():
         local_time = date.replace(tzinfo=pytz.utc).astimezone(local_tz)
         converted_dates.append(local_time)
 
-# --------------------- TASK 3 ------------------------------------------------
-
-
-def preprocessing():
-    file = open("birds.txt", "r")  # Öppnar filen birds och läser den.
-    listG = []  # Skapar en tom lista
-    for line in file:
-        listG.append(re.findall(r"[\w']+", line))
-
-    listindex = []
-    for a in range(len(listG)):
-        listG[a-1] = [int(i) for i in listG[a-1]]  # makes them into integers
-        listG[a] = [int(i) for i in listG[a]]
-        diff = listG[a][7] - listG[a-1][7]
-
-        if a > 0:
-            if diff < 0:
-                listindex.append(a)
-    for i in range(len(listindex)):
-        del listG[listindex[i]]  # we do this to delete the unwanted lines
-
-    difflist = []
-    dateslist = []
-
-    for a in range(len(listG)):
-        listG[a-1] = [int(i) for i in listG[a-1]]  # makes them into integers
-        listG[a] = [int(i) for i in listG[a]]
-        # datelist is a list of the dates as integers
-        dateslist.append(listG[a][0:6])
-        diff = listG[a][7]-listG[a-1][7]
-        difflist.append(diff)
-
-    for t in range(len(difflist)):
-        if difflist[t] < 0:
-            difflist[t] = 0  # changes difference to be 0 when there is a reset
-        if difflist[t] > 8:
-            difflist[t] = 8
-
-    A = 0
-    datalist = []
-
-    for a in range(len(difflist)):
-        A = difflist[a] + A
-        datalist.append(A)
-
-    finaldatalist = [k+70 for k in datalist]  # list comprehention
-    print(finaldatalist[1470])
-    print(listG[1470])
-
-    finaldateslist = []
-    for object in listG:
-        frmt = '%Y-%m-%d %H:%M:%S.%f'
-        a, b, c = line.split()
-        finaldateslist.append(datetime.strptime(a + " " + b, frmt))
-
-    print(finaldateslist[1470])
 
 # --------------------- TASK 4 ------------------------------------------------
 
@@ -126,7 +71,7 @@ def collect_plot_dates(start_date):
         if (start_date in str(i)):
             index_list.append(list_dates.index(i))
 
-    for i in list_dates:
+    for i in converted_dates:
         a, b = str(i).split()
         if (start_date in a):
             plot_dates.append(i)
@@ -197,7 +142,17 @@ def day_night_cycle():
 if __name__ == '__main__':
     # read_file()
     # convert_local_timezone()
-    preprocessing()
+    list_dates, list_data = preprocessing()
+    convert_local_timezone()
     # graph_dates, graph_data = compute_data()
     # plot_graph(graph_dates, graph_data)
     # day_night_cycle()
+    
+    print(list_dates[0])
+    print(list_dates[4000])
+    print(list_dates[8000])
+    print(list_dates[30000])
+    print(converted_dates[0])
+    print(converted_dates[4000])
+    print(converted_dates[8000])
+    print(converted_dates[30000])
