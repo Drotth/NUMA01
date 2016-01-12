c22f729655df2b4a8c45c5250f41314b194f351b
Andreas Drotth <drotth@hotmail.com>
2016-01-13 00:05:09 +0100

    Index error solved, as well as beginning of new collect-function.

--- Project/project_3_birds.py
+++ Project/project_3_birds.py
@@ -36,6 +36,7 @@ def compute_data():
     date_1 = datetime.strptime(start_date, "%Y-%m-%d")
 
     collect_plot_dates(start_date)  # collect dates/data for start date
+    # collect_plot_dates2(start_date, days)
 
     if int(days) > 1:  # Repeat collection for each plus day separatly
         for t in range(1, int(days)):
@@ -50,6 +51,45 @@ def compute_data():
     return graph_dates, graph_data
 
 
+def collect_plot_dates2(start_date, days):
+    first = 0
+    last = 0
+    index_list = []
+    date_found = False
+    
+    date_1 = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = date_1 + timedelta(int(days))
+
+#    for i in list_dates:
+#        if (start_date in str(i)):
+#            index_list.append(list_dates.index(i))
+
+    for i in list_dates:
+        a, b = str(i).split()
+        
+        if (date_found == False and start_date in a):
+            # print("FOUND IT AT INDEX:", list_dates.index(i))
+            date_found = True
+            plot_dates.append(i)
+            first = list_dates.index(i)
+        elif(date_found == True):
+            # print("APPENDING INDEX:", list_dates.index(i))
+            plot_dates.append(i)
+            if (end_date.strftime('%Y-%m-%d') in a):
+                # print("STOPPING AT INDEX:", list_dates.index(i))
+                last = list_dates.index(i)
+                break
+        last = list_dates.index(i)
+
+    # first = index_list[0]
+    # last = index_list[-1]
+
+    print("first is:", first)
+    print("last is:", last)    
+    
+    collect_plot_data(first, last)
+
+
 def collect_plot_dates(start_date):
     first = 0
     last = 0
@@ -59,7 +99,7 @@ def collect_plot_dates(start_date):
         if (start_date in str(i)):
             index_list.append(list_dates.index(i))
 
-    for i in converted_dates:
+    for i in list_dates:
         a, b = str(i).split()
         if (start_date in a):
             plot_dates.append(i)
@@ -83,8 +123,11 @@ def graph_values(diff_array):
 
     graph_dates.append(plot_dates[0].strftime('%Y-%m-%d'))
 
+    print("Length of diff_array", len(diff_array))
+    print("Length of plot_dates", len(plot_dates))
+    print("Length of plot_data", len(plot_data))
+
     for data in diff_array:
-        # print("index is:", index, "data is",int(data))
         if (plot_dates[index].hour > current_hour):
             graph_data.append(sum_value)
             sum_value = 0
@@ -96,7 +139,7 @@ def graph_values(diff_array):
             sum_value = 0
             sum_value = sum_value + int(data)
             current_hour = 0
-            graph_dates.append(plot_dates[0].strftime('%Y-%m-%d'))
+            graph_dates.append(plot_dates[index].strftime('%Y-%m-%d'))
         else:
             sum_value = sum_value + int(data)
 
@@ -113,6 +156,7 @@ def plot_graph(graph_dates, graph_data):
     x_values = np.array(range(0, len(graph_dates)))
     y_values = np.array(graph_data)
     x_names = graph_dates
+    print(x_names)
 
     ax = plt.subplot(111)
     plt.xticks(rotation=90)  # Roterar det som står på x-axeln
@@ -126,7 +170,6 @@ def plot_graph(graph_dates, graph_data):
 
 def day_night_cycle():
     print("Visualize day and night cycle")
-
 
 if __name__ == '__main__':
     list_dates, list_data = preprocessing("birds.txt")
