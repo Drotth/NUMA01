6807dabd32cbb681379a9bb6671c21d387f85fea
Andreas Drotth <drotth@hotmail.com>
2016-01-13 13:27:44 +0100

    Managed to plot double plots!

--- Project/project_3_birds.py
+++ Project/project_3_birds.py
@@ -8,7 +8,10 @@ from pylab import *
 from datetime import datetime
 from datetime import timedelta
 import pytz
+from astral import Astral
 from preprocess import preprocessing
+import numpy as np
+import matplotlib.pyplot as plt
 
 list_dates = []
 list_data = []
@@ -31,8 +34,8 @@ def convert_local_timezone():
 
 
 def compute_data():
-    start_date = input('Start date [YYYY-MM-DD]: ')
-    days = input('Number of days: ')
+    start_date = '2015-01-26' #input('Start date [YYYY-MM-DD]: ')
+    days = '1' #input('Number of days: ')
     date_1 = datetime.strptime(start_date, "%Y-%m-%d")
 
     collect_plot_dates(start_date)  # collect dates/data for start date
@@ -45,9 +48,9 @@ def compute_data():
 
     array1 = np.array(plot_data)
     diff_array = np.diff(array1)
-    graph_dates, graph_data = graph_values(diff_array)
+    graph_dates, graph_data, sun_indexes = graph_values(diff_array)
 
-    return graph_dates, graph_data
+    return graph_dates, graph_data, sun_indexes
 
 
 def collect_plot_dates(start_date):
@@ -80,8 +83,18 @@ def graph_values(diff_array):
     sum_value = 0
     index = 0
     current_hour = plot_dates[0].hour
+    sun_indexes = []
 
     graph_dates.append(plot_dates[0].strftime('%Y-%m-%d'))
+
+    city_name = 'Stockholm'
+    a = Astral()
+    a.solar_depression = 'civil'
+    city = a[city_name]
+
+    sunrise_found = False
+    sunset_found = False
+    sun = city.sun(plot_dates[0], local=True)
 
     for data in diff_array:
         if (plot_dates[index].hour > current_hour):
@@ -96,39 +109,61 @@ def graph_values(diff_array):
             sum_value = sum_value + int(data)
             current_hour = 0
             graph_dates.append(plot_dates[index].strftime('%Y-%m-%d'))
+            sunrise_found = False
+            sunset_found = False
+            sun = city.sun(plot_dates[index], local=True)
         else:
             sum_value = sum_value + int(data)
+
+        if (plot_dates[index].hour == sun['sunrise'].hour and sunrise_found == False):
+            sun_indexes.append(current_hour)
+            sunrise_found = True
+        if (plot_dates[index].hour == sun['sunset'].hour and sunset_found == False):
+            sun_indexes.append(current_hour)
+            sunset_found = True
 
         index = index + 1
 
     graph_data.append(sum_value)
 
-    return graph_dates, graph_data
+    print(sun_indexes)
+
+    return graph_dates, graph_data, sun_indexes
 
 # --------------------- TASK 5 ------------------------------------------------
 
 
-def plot_graph(graph_dates, graph_data):
+def plot_graph(graph_dates, graph_data, sun_indexes):
     x_values = np.array(range(0, len(graph_dates)))
     y_values = np.array(graph_data)
     x_names = graph_dates
-
-    ax = plt.subplot(111)
+    
+    
+    x2_values = np.array(range(0, len(graph_dates)))
+    y2_raw = [0,0,0,0,0,0,0,0,12,12,12,12,12,12,12,12,0,0,0,0,0,0,0,0]
+    y2_values = np.array(y2_raw)
+    
     plt.xticks(rotation=90)  # Roterar det som står på x-axeln
     plt.xticks(x_values, x_names)
     barWidth = 1  # Bredd på staplarna
-    ax.bar(x_values, y_values, width=barWidth, align='center')
+    plt.bar(x2_values, y2_values, width=barWidth, align='center', color='y')
+    plt.bar(x_values, y_values, width=barWidth, align='center')
     plt.show()
 
 # --------------------- TASK 6 ------------------------------------------------
 
 
 def day_night_cycle():
-    print("Visualize day and night cycle")
+    city_name = 'Stockholm'
+    a = Astral()
+    a.solar_depression = 'civil'
+    city = a[city_name]
+    
+    sun = city.sun(plot_dates[0], local=True)
 
 if __name__ == '__main__':
     list_dates, list_data = preprocessing("birds.txt")
     convert_local_timezone()
-    graph_dates, graph_data = compute_data()
-    plot_graph(graph_dates, graph_data)
-    # day_night_cycle()
+    graph_dates, graph_data, sun_indexes = compute_data()
+    plot_graph(graph_dates, graph_data, sun_indexes)
+    day_night_cycle()
